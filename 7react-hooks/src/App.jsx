8474e8d9625154c196bc96aa28298da4ce63ac77
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2026-06-04 23:49:54 +0530

    understading hooks

--- 7react-hooks/src/App.jsx
+++ 7react-hooks/src/App.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 
-function Counter() {
+const App = () => {
   const [count, setCount] = useState(0);
 
+  function increment(){
+    setCount(count + 1)
+  }
+
+  function decrement(){
+    setCount(count - 1)
+  }
+
+  function jumpBy5(){
+    setCount(count+5)
+  }
+
+  function fallBy5(){
+    setCount(count-5)
+  }
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={jumpBy5}>Jump By 5</button>
+      <button onClick={fallBy5}>Fall By 5</button>
     </div>
   );
 }
 
-export default Counter;
+export default App;
